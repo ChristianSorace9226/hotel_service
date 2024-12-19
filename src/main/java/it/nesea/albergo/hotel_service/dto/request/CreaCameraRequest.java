4e4c4e557b36d597f3cb76cc0c0eff66ec72330a
ChristianSorace9226 <soracechristian0@gmail.com>
2024-12-19 14:05:42 +0100

    fix/ remove comments

--- src/main/java/it/nesea/albergo/hotel_service/dto/request/CreaCameraRequest.java
+++ src/main/java/it/nesea/albergo/hotel_service/dto/request/CreaCameraRequest.java
@@ -11,7 +11,6 @@ import lombok.experimental.FieldDefaults;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
@@ -31,12 +30,6 @@ public class CreaCameraRequest implements Serializable {
 
     @NotNull(message = "lo stato della camera deve essere definito")
     Integer idStato;
-
-//    @NotNull(message = "capacita non può essere null")
-//    Integer capacita;
-
-//    @NotNull(message = "prezzo per notte non può essere null")
-//    BigDecimal prezzoPerNotte;
 
     @NotNull(message = "data inizio disponibilita non può essere null")
     LocalDate dataInizioDisponibilita;
